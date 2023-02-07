4c37e2a28d91a430fa34f80582d57122b0a3b526
boiledEgg-s <seokjun2000@gmail.com>
2023-02-07 20:06:51 +0900

    Second Login Commit, API Connected

--- app/src/main/java/com/softsquared/template/kotlin/src/splash/SplashActivity.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/splash/SplashActivity.kt
@@ -7,12 +7,14 @@ import android.os.Looper
 import com.softsquared.template.kotlin.config.BaseActivity
 import com.softsquared.template.kotlin.databinding.ActivitySplashBinding
 import com.softsquared.template.kotlin.src.main.MainActivity
+import com.softsquared.template.kotlin.src.main.login.LoginActivity
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Handler(Looper.getMainLooper()).postDelayed({
+            //startActivity(Intent(this, LoginActivity::class.java))
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 1500)
